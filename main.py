1f6151a377cd95aac159aa738b825b9e9a1a03af
leastchaos <leastchaos@gmail.com>
2021-03-24 23:10:24 +0800

    final update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.data import convert_csv_to_report
-from src.sheet import update_worksheet
+from src.sheet import update_data_worksheet,update_review_worksheet
 
 if __name__=="__main__":
     path=r'data\Trades2.csv'
     df=convert_csv_to_report(path)
-    update_worksheet(df)
+    update_data_worksheet(df)
+    update_review_worksheet(df)
     print('complete')
